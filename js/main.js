9eb807e9235dbf458ef4f8c592a9b6ecd0184c7b
Carryl Sorene <carrylsorene@gmail.com>
2015-08-18 20:19:00 +0100

    HTML dropdown and some more JS

---
+++ js/main.js
@@ -0,0 +1,101 @@
+
+var trainLines = {
+ lineN: ['Times Square', '34th', '28thOnN', '23rdOnN', 'Union SquareOnN', '8thOnN'],          // 6 stops, intersect at index[4]
+ lineL: ['8thOnL', '6th', 'Union SquareOnL', '3rd', '1st'],                                // 5 stops, intersect at index[2]
+ line6: ['Grand Central', '33rd', '28thOn6', '23rdOn6', 'Union SquareOn6', 'Astor Place']     // 6 stops, intersect at index[4]
+};
+
+  var start = document.getElementById('start');
+  var end = document.getElementById('end')
+    console.log('Hi');
+
+
+  end.addEventListener('change', function(e).value{
+    console.log('value eventListener')
+  };
+
+  trainLines.lineN.indexOf(start.value);
+  trainLines.lineN.indexOf(end.value);
+
+// start.addEventListener('change', function(){
+// });
+// end.addEventListener('change', function(){
+// }):
+
+var stops = indexOf.end - indexOf.start;
+
+// if (indexOf.start > indexOf.end) {
+//   indexOf.end - indexOf.start // because you find index of start and end stations by looking up their index
+//   return stops //because end minus start gives you number of stops
+// } 
+
+// if (lineL.indexOf
+//   indexOf.start > indexOf.end){
+//   indexOf.end - indexOf.start // because you find index of start and end stations by looking up their index
+//   return stops //because end minus start gives you number of stops
+// } 
+
+// if (line6.indexOf
+//   indexOf.start > indexOf.end){
+//   indexOf.end - indexOf.start // because you find index of start and end stations by looking up their index
+//   return stops //because end minus start gives you number of stops
+// } 
+
+var stops = //difference between start and end ie, end minus start
+  end - start;
+var changes = 
+if //selection includes [lineN,4],[lineL,2], [lineN,4], then change lines - return value of stop and new line chosen - will always be 1 change cos only 1 intersection
+
+
+tripLength function(){
+  return ('Your journey will include ' + stops + 'stops and ' + changes 'changes.')
+  console.log
+}
+
+//store results in array? like in tictactoe
+
+//the intersection is the thing - once they choose a route that changes there, they fork off and you add the rest of their stop numbers from that line - so change to that line if the intersection is chosen -
+//if stop x is clicked/chosen, split/splice ie go from there ?
+
+//output it just to text or store in array?
+//var = current choice depending on user's selection from dropdown/input
+
+  // <option id="id1" value="value1">Times Square on Line N</option> 
+  // <option id="id2" value="value2">34th on Line N</option>
+  // <option id="id3" value="value3">28th on Line N</option>
+  // <option id="id4" value="value4">23rd on Line N</option> 
+  // <option id="id5" value="value5">Union Square on Line N</option>
+  // <option id="id6" value="value6">8th on Line N</option>
+
+  // <option id="id7" value="value7">8th on Line L</option> 
+  // <option id="id8" value="value8">6th on Line L</option>
+  // <option id="id9" value="value9">Union Square on Line L</option>
+  // <option id="id10" value="value10">3rd on Line L</option> 
+  // <option id="id11" value="value11">1st on Line L</option>
+
+  // <option id="id12" value="value12">Grand Central on Line 6</option>
+  // <option id="id13" value="value13">33rd on Line 6</option> 
+  // <option id="id14" value="value14">28th on Line 6</option>
+  // <option id="id15" value="value15">23rd on Line 6</option>
+  // <option id="id16" value="value16">Union Square on 6</option> 
+  // <option id="id17" value="value17">Astor Place on Line 6</option>
+
+  // <option id="id18" value="value1">Times Square on Line N</option> 
+  // <option id="id19" value="value2">34th on Line N</option>
+  // <option id="id20" value="value3">28th on Line N</option>
+  // <option id="id21" value="value4">23rd on Line N</option> 
+  // <option id="id22" value="value5">Union Square on Line N</option>
+  // <option id="id23" value="value6">8th on Line N</option>
+
+  // <option id="id24" value="value7">8th on Line L</option> 
+  // <option id="id25" value="value8">6th on Line L</option>
+  // <option id="id26" value="value9">Union Square on Line L</option>
+  // <option id="id27" value="value10">3rd on Line L</option> 
+  // <option id="id28" value="value11">1st on Line L</option>
+
+  // <option id="id29" value="value12">Grand Central on Line 6</option>
+  // <option id="id30" value="value13">33rd on Line 6</option> 
+  // <option id="id31" value="value14">28th on Line 6</option>
+  // <option id="id32" value="value15">23rd on Line 6</option>
+  // <option id="id33" value="value16">Union Square on 6</option> 
+  // <option id="id34" value="value17">Astor Place on Line 6</option>
